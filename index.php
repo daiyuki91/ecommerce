d00224a913bdea3771560c6803a302050bfd700d
Daiyuki <d.maiafujiyoshi@gmail.com>
2020-09-14 15:29:59 -0300

    criando PageAdmin e o tplAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -20,6 +21,14 @@ $app->get('/', function() {
 	*/
 	
 	$page = new Page();
+	$page->setTpl("index");
+	
+});
+
+//rota admin
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 	
 });
