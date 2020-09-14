c7bc94cc9cfccda9d27a9195c2311a19bfc58b6d
Daiyuki <d.maiafujiyoshi@gmail.com>
2020-09-14 12:58:34 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -2,21 +2,28 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 //rota principal
 $app->get('/', function() {
     
+	/*
+	//teste inicial
 	$sql = new Hcode\DB\Sql();
-	
 	$results = $sql->select("SELECT * FROM tb_users");
-	
 	echo json_encode($results);
-
+	*/
+	
+	$page = new Page();
+	$page->setTpl("index");
+	
 });
 
-$app->run();
+$app->run(); //faz rodar tudo que está na memória
 
  ?>
