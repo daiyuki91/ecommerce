c0733ac80b70feb3c975f2521a6618de405c9aa4
Daiyuki <d.maiafujiyoshi@gmail.com>
2020-09-15 16:02:14 -0300

    versão do PHPMailer atualizada e implementação do mecanismo de recuperação de senha

--- index.php
+++ index.php
@@ -59,13 +59,191 @@ $app->post('/admin/login', function() {
 	
 });
 
-//rota login - logout
+//rota logout
 $app->get('/admin/logout', function() {
     
 	User::logout();
 	
 	header("Location: /admin/login");
 	exit;
+	
+});
+
+//rota tela - listar todos os usuários
+$app->get('/admin/users', function() {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	$users = User::listAll(); //array com lista de usuários
+	
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+	
+});
+
+//rota tela - criar usuário
+$app->get('/admin/users/create', function() {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+	
+});
+
+//rota tela - excluir usuário
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	$user = new User();
+	$user->get((int)$iduser);
+	
+	//deletando usuário
+	$user->delete();
+	
+	header("Location: /admin/users");
+	exit;
+	
+});
+
+//rota tela - alterar usuário
+$app->get('/admin/users/:iduser', function($iduser) {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	$user = new User();
+	$user->get((int)$iduser);
+	
+	$page = new PageAdmin();
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+	
+});
+
+//rota tela - salvar novo usuário
+$app->post('/admin/users/create', function() {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	//var_dump($_POST); //visualizar os dados recebidos pelo método "post" (vindo do formulário HTML)
+	
+	//insert do usuário
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	
+	$user->setData($_POST);	
+	$user->save();
+	
+	header("Location: /admin/users");
+	exit;
+	
+	//var_dump($user); //visualizar a criação dos objetos com o nome dos atributos da tabela
+	
+	
+});
+
+//rota tela - salvar edição do usuário
+$app->post('/admin/users/:iduser', function($iduser) {
+    
+	User::verifyLogin(); //verificar se está logado no admin
+	
+	//update do usuário
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+	
+	header("Location: /admin/users");
+	exit;
+	
+});
+
+//rota tela - forgot (solicitando a recuperação de senha)
+$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot");
+
+});
+
+//rota tela - forgot-sent (coletando o email de recuperação)
+$app->post("/admin/forgot", function() {
+	
+	$user = User::getForgot($_POST["email"]);
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+	
+});
+
+//rota tela - forgot-sent (confirmação do envio do email de redefinição de senha)
+$app->get("/admin/forgot/sent", function() {
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot-sent");
+	
+});
+
+//rota tela - forgot-reset (redefinição de senha)
+$app->get("/admin/forgot/reset", function() {
+	
+	$user = User::validForgotDecrypt($_GET["code"]);
+	
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+	
+});
+
+//rota tela - forgot-reset (informar a nova senha)
+$app->post("/admin/forgot/reset", function() {
+	
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+	
+	//registrar a solicitação da mudança de senha para verificar se o link de recuperação foi usado dentro de 1 hora
+	User::setForgotUsed($forgot["idrecovery"]);
+	
+	//carregando os dados do usuário
+	$user = new User();
+	$user->get((int)$forgot["iduser"]);
+	
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+	
+	//este método é usado para criar um hash para a senha
+	$user->setPassword($password);
+	
+	//confirmação visual da alteração da senha
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("forgot-reset-success");
 	
 });
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,10 +4,13 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 class User extends Model {
 	
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret"; //chave com 16 caracteres
+	const SECRET_IV = "HcodePhp7_Secret_IV"; //chave com 16 caracteres IV
 	
 	public static function login($login, $password) {
 		
@@ -46,7 +49,7 @@ class User extends Model {
 		}
 		
 	}
-	/*
+	
 	public static function verifyLogin($inadmin = true)
 	{
 		
@@ -66,31 +69,240 @@ class User extends Model {
 		}
 		
 	}
-	*/
-	public static function verifyLogin($inadmin = true)
-	{
-		
-		if (
-			!isset($_SESSION[User::SESSION])						
-			||
-			!$_SESSION[User::SESSION]								
-			||
-			!(int)$_SESSION[User::SESSION]["iduser"] > 0			
-			||
-			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin	
-		) {
-			
-			header("Location: /admin/login");
-			exit;
-			
-		}
-		
-	}
 	
 	public static function logout()
 	{
 		
 		$_SESSION[User::SESSION] = NULL;
+		
+	}
+	
+	public static function listAll()
+	{
+		
+		$sql = new Sql();
+		
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson;");
+		
+	}
+	
+	public function get($iduser) {
+		
+		$sql = new Sql(); //criando uma instância Sql para acessar o banco de dados
+		
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+			":iduser"=>$iduser
+		));
+		
+		$this->setData($results[0]);
+		
+	}
+	
+	public function save()
+	{
+		
+		$sql = new Sql();
+		
+		/*
+			//Ordem de variáriveis de entrada da procedure sp_user_save
+			pdesperson VARCHAR(64), 
+			pdeslogin VARCHAR(64), 
+			pdespassword VARCHAR(256), 
+			pdesemail VARCHAR(128), 
+			pnrphone BIGINT, 
+			pinadmin TINYINT
+		*/
+		
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+			array(
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+				)
+		); //chamar uma procedure para inserir um novo usuário (só com uma chamada, requisição)
+		
+		$this->setData($results[0]);
+		
+	}
+	
+	public function update()
+	{
+		
+		$sql = new Sql();
+		
+		/*
+			//Ordem de variáriveis de entrada da procedure sp_userupdate_save
+			piduser INT,
+			pdesperson VARCHAR(64), 
+			pdeslogin VARCHAR(64), 
+			pdespassword VARCHAR(256), 
+			pdesemail VARCHAR(128), 
+			pnrphone BIGINT, 
+			pinadmin TINYINT
+		*/
+		
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+			array(
+				":iduser"=>$this->getiduser(),
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+				)
+		); //chamar uma procedure para inserir um novo usuário (só com uma chamada, requisição)
+		
+		$this->setData($results[0]);
+		
+	}
+	
+	public function delete()
+	{
+		
+		$sql = new Sql();
+		
+		/*
+			//Ordem de variáriveis de entrada da procedure sp_users_delete
+			piduser INT
+		*/
+		
+		$sql->query("CALL sp_users_delete(:iduser)",
+			array(
+				":iduser"=>$this->getiduser()
+			)
+		);
+		
+	}
+	
+	public static function getForgot($email)
+	{
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;",
+			array(
+					":email"=>$email
+			)		
+		);
+		
+		//verificar se o email está cadastrado no banco de dados
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			//criar um novo registro na tabela de recuperação de senhas
+			
+			
+			$data = $results[0];
+			
+			/*
+				//Ordem de variáriveis de entrada da procedure sp_userspasswordsrecoveries_create
+				piduser INT,
+				pdesip VARCHAR(45)
+			*/
+			
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)",
+				array(
+					":iduser"=>$data["iduser"],
+					":desip"=>$_SERVER["REMOTE_ADDR"]
+				)
+			);
+			
+			if (count($results2) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha.");
+			}
+			else
+			{
+				$dataRecovery = $results2[0];
+				
+				//gerar um código criptografado
+				//1- criar os dados em base64
+				
+				$code = openssl_encrypt($dataRecovery["idrecovery"], 'AES-128-CBC', pack("a16",User::SECRET), 0, pack("a16",User::SECRET_IV));
+				$code = base64_encode($code);
+				
+				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				
+				$mailer = new Mailer($data["desemail"],$data["desperson"],"Redefinir senha da Hcode Store", "forgot",
+					array(
+						"name"=>$data["desperson"],
+						"link"=>$link
+					)
+				);
+				
+				$mailer->send();
+				
+				return $data;
+				
+			}
+		}
+		
+	}
+	
+	public static function validForgotDecrypt($code)
+	{
+		
+		$idrecovery = openssl_decrypt(base64_decode($code), 'AES-128-CBC', pack("a16",User::SECRET), 0, pack("a16",User::SECRET_IV));
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR ) >= NOW();
+		", array(
+			":idrecovery"=>$idrecovery
+		));
+		
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			return $results[0];
+		}
+		
+	}
+	
+	public static function setForgotUsed($idrecovery)
+	{
+		
+		$sql = new Sql();
+		
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery",
+			array(
+				":idrecovery"=>$idrecovery
+			)
+		);
+		
+	}
+	
+	public function setPassword($password)
+	{
+		
+		$sql = new Sql();
+		
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser",
+			array(
+				":password"=>$password,
+				":iduser"=>$this->getiduser()
+			)
+		);
 		
 	}
 	
