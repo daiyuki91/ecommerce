2d24013018a142d0be8dbe3e2d2cbe5618f4de11
Daiyuki <d.maiafujiyoshi@gmail.com>
2020-09-21 16:32:05 -0300

    Admin - Pedidos - Busca e Paginacao

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -11,6 +11,8 @@ class Order extends Model {
 	const SUCCESS = "OrderSuccess";
 	const ERROR = "OrderError";
 	
+	const ITEMS_PER_PAGE = 10;
+	
 	public function save()
 	{
 		
@@ -157,6 +159,67 @@ class Order extends Model {
 	{
 		
 		$_SESSION[Order::SUCCESS] = NULL;
+		
+	}
+	
+	public static function getPage($page = 1, $itemsPerPage = User::ITEMS_PER_PAGE)
+	{
+		
+		$start = ($page-1) * $itemsPerPage;
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			ORDER BY a.dtregister DESC
+			LIMIT $start, $itemsPerPage;
+		");
+		
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //função do PHP para arredondar para cima
+		];
+	}
+	
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = User::ITEMS_PER_PAGE)
+	{
+		
+		$start = ($page-1) * $itemsPerPage;
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			WHERE a.idorder = :id OR f.desperson LIKE :search
+			ORDER BY a.dtregister DESC
+			LIMIT $start, $itemsPerPage;
+		", [
+			":search"=>'%'.$search.'%',
+			":id"=>'%'.$search.'%'
+		]);
+		
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //função do PHP para arredondar para cima
+		];
 		
 	}
 	
